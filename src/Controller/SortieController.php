5191201bd04e08421aadebd9deb2b0e4e29ed442
paul <paul.lecerf87@gmail.com>
2023-02-02 16:00:08 +0100

    enlever ligne 82

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -11,6 +11,7 @@ use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -127,7 +128,7 @@ class SortieController extends AbstractController
         }
         return $this->render('sorties/annuler.html.twig',[
             "sortie"=> $sortie,
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
     }
 }
